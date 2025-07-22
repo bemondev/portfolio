28d67526d201a8e66b14adc62432c7e77f4e9bda
Bernardo Montaña <bemondev@gmail.com>
2025-07-21 23:55:00 -0300

    trying to add open graph preview of the site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,31 @@ const lexendDeca = Lexend_Deca({
 })
 
 export const metadata: Metadata = {
-  title: "Portfolio @bemondev",
-  description: "Personal web portfolio of @bemondev",
+  title: "@bemondev | Portfolio",
+  description: "Personal portfolio of Bernardo Montaña. Showcasing my projects, skills and development style",
+  openGraph: {
+    title: "@bemondev | Portfolio", 
+    description: "Personal portfolio of Bernardo Montaña. Showcasing my projects, skills and development style",
+    url: "https://www.bemon.dev", 
+    siteName: "@bemondev | Portfolio",
+    images: [
+      {
+        url: "https://www.bemon.dev/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Preview de Bemondev Portfolio", 
+      },
+    ],
+    locale: "en",
+    type: "website", 
+  },
+  twitter: {
+    card: "summary_large_image", 
+    creator: "@bemondev",
+    title: "@bemondev | Portfolio",
+    description: "Personal portfolio of Bernardo Montaña. Showcasing my projects, skills and development style",
+    images: ["https://www.bemon.dev/preview.png"],
+  },
 };
 
 export default function RootLayout({
